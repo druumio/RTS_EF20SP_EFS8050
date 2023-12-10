d09ef8cbbb3bc38222405c512b0ba84d3830f0be
druumio <druumio@gmail.com>
2023-12-11 00:40:33 +0200

    Started task 6

--- Coding_exercise_6/AutomatedGardeningSystem/src/main.cpp
+++ Coding_exercise_6/AutomatedGardeningSystem/src/main.cpp
@@ -1,18 +1,70 @@
+/*
+Libraries
+*/
 #include <Arduino.h>
+#include <Arduino_FreeRTOS.h>
+#include "semphr.h"
+#include "task.h"
+#include <queue.h>
+#include <event_groups.h>
+#include <queue.h>
+#include <timers.h>
+/*
+Definitions
+*/
+#define LEDPIN PB5 // 11
+/*
+Globals
+*/
+SemaphoreHandle_t xSerialSemaphore;
+/*
+Function declarations
+*/
+void SoilMoistureTask(void *pvParameters);
+void LightManagementTask(void *pvParameters);
+void WaterControlTask(void *pvParameters);
+void SensorSimulationTask(void *pvParameters);
+void UserInputTask(void *pvParameters);
+void ReportTask(void *pvParameters);
+void setup(void);
+void loop(void);
+/*
+Function Definitions
+*/
+void setup(void)
+{
+  // Setup Serial and related semaphore
+  Serial.begin(9600);
+  Serial.println("Setup Start");
+  if (xSerialSemaphore == NULL)
+  {
+    xSerialSemaphore = xSemaphoreCreateMutex();
+    if ((xSerialSemaphore) != NULL)
+      xSemaphoreGive((xSerialSemaphore));
+  }
+  /*
 
-// put function declarations here:
-int myFunction(int, int);
+  Something
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  */
+  Serial.println("Starting Task Scheduler");
+  vTaskStartScheduler();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(void)
+{
+  // Nothing to see here
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void SoilMoistureTask(void *pvParameters)
+{
+  /*
+  Setup for this task
+  */
+  for (;;)
+  {
+    /*
+    Running tasks
+    */
+  }
 }
